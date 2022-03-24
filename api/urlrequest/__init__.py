b05bfbed6e65589fac9bce32c53cb9d763a111d7
Cykalele <hauserthommy@web.de>
2022-03-24 20:47:32 +0100

    post

--- api/urlrequest/__init__.py
+++ api/urlrequest/__init__.py
@@ -1,6 +1,7 @@
 import logging
 import azure.functions as func
-"""
+
+
 from urllib.parse import parse_qs
 
 
@@ -44,3 +45,4 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             "Please pass a name on the query string or in the request body",
             status_code=400
         )
+"""
