c295298a75f31bf62455b72394f62c0f36a65555
Cykalele <hauserthommy@web.de>
2022-03-24 20:32:30 +0100

    ggg

--- api/urlrequest/__init__.py
+++ api/urlrequest/__init__.py
@@ -1,7 +1,30 @@
 import logging
 import azure.functions as func
 
+from urllib.parse import parse_qs
 
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    # This function will parse the response of a form submitted using the POST method
+    # The request body is a Bytes object
+    # You must first decode the Bytes object to a string
+    # Then you can parse the string using urllib parse_qs
+
+    logging.info("Python HTTP trigger function processed a request.")
+    req_body_bytes = req.get_body()
+    logging.info(f"Request Bytes: {req_body_bytes}")
+    req_body = req_body_bytes.decode("utf-8")
+    logging.info(f"Request: {req_body}")
+
+    long_url = parse_qs(req_body)["long_url"][0]
+
+
+    return func.HttpResponse(
+        f"You submitted this information: {long_url}",
+        status_code=200,
+    )
+
+"""
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
@@ -21,3 +44,4 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             "Please pass a name on the query string or in the request body",
             status_code=400
         )
+"""
