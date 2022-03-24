3e70af02ddfbbdb3f573d36f6f2d992ce3683480
Cykalele <hauserthommy@web.de>
2022-03-24 17:37:29 +0100

    http

--- api/HttpTrigger1/__init__.py
+++ api/HttpTrigger1/__init__.py
@@ -18,6 +18,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     if url:
         return func.HttpResponse(f"Hello, {url}. This HTTP triggered function executed successfully.")
     else:
+        
         return func.HttpResponse(
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
              status_code=200
