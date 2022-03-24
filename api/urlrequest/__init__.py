65eccee24165ef281550e9f32d82b466396797f5
Cykalele <hauserthommy@web.de>
2022-03-24 20:08:02 +0100

    dd

--- api/urlrequest/__init__.py
+++ api/urlrequest/__init__.py
@@ -3,35 +3,10 @@ import logging
 import azure.functions as func
 from urllib.parse import parse_qs
 
-def main(req: func.HttpRequest) -> func.HttpResponse:
-    try:
-        req_body = req.get_json()
-        return func.HttpResponse(json.dumps([
-            {
-                'action': 'talk',
-                'text': 'Thank you for telling us how you feel.',
-            },
-        ]), mimetype='application/json')
-    except ValueError:
-        return func.HttpResponse(
-            "Could not parse request body.",
-            status_code=400
-        )
 
-"""
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info("Python HTTP trigger function processed a request.")
 
-    req_body_bytes = req.get_body()
-    logging.info("Request Bytes: {req_body_bytes}")
-    req_body = req_body_bytes.decode("utf-8")
-    logging.info("Request Bytes: {req_body}")
-
-    long_url = parse_qs(req_body)["long_url"][0]
-
-    return func.HttpResponse(
-        "You submitted this information: {long_url}", status_code=200
-    )
     url = req.params.get('long_url')
     if not url:
         try:
@@ -49,4 +24,3 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
              status_code=200
         )
-"""
