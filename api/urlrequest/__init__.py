204bad87e3df3be2cdeba8bd4d505da77833be6a
Cykalele <hauserthommy@web.de>
2022-03-24 19:37:21 +0100

    tt

--- api/urlrequest/__init__.py
+++ api/urlrequest/__init__.py
@@ -1,12 +1,23 @@
 import logging
 
 import azure.functions as func
+from urllib.parse import parse_qs
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    url = req.params.get('long_url')
+    req_body_bytes = req.get_body()
+    logging.info('Request Bytes: {req_body_bytes}')
+    req_body = req_body_bytes.decode('utf-8')
+    logging.info('Request Bytes: {req_body}')
+
+    long_url = parse_qs(req_body)['long_url'][0]
+
+    return func.HttpResponse(
+        'You submitted this information: {long_url}', status_code=200
+    )
+    """url = req.params.get('long_url')
     if not url:
         try:
             req_body = req.get_json()
@@ -22,4 +33,4 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         return func.HttpResponse(
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
              status_code=200
-        )
+        )"""
