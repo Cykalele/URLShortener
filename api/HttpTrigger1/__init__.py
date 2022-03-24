d96cf2d85ce5251e126c4d9206c346b67c1647d6
Cykalele <hauserthommy@web.de>
2022-03-24 17:33:08 +0100

    http trigger

---
+++ api/HttpTrigger1/__init__.py
@@ -0,0 +1,24 @@
+import logging
+
+import azure.functions as func
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    url = req.params.get('long_url')
+    if not url:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            url = req_body.get('long_url')
+
+    if url:
+        return func.HttpResponse(f"Hello, {url}. This HTTP triggered function executed successfully.")
+    else:
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
