20586c0a896fe1d480390c4a570fd178e5b814f7
Cykalele <hauserthommy@web.de>
2022-03-24 20:29:29 +0100

    dd

--- api/urlrequest/__init__.py
+++ api/urlrequest/__init__.py
@@ -5,17 +5,17 @@ import azure.functions as func
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
+    long_url = req.params.get('long_url')
     if not name:
         try:
             req_body = req.get_json()
         except ValueError:
             pass
         else:
-            name = req_body.get('name')
+            name = req_body.get('long_url')
 
     if name:
-        return func.HttpResponse(f"Hello {name}!")
+        return func.HttpResponse(f"Hello {long_url}!")
     else:
         return func.HttpResponse(
             "Please pass a name on the query string or in the request body",
