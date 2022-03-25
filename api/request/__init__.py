198bbcd45f7d42bc173ac056fcd13983407074a2
Cykalele <hauserthommy@web.de>
2022-03-25 10:20:25 +0100

    change

--- api/request/__init__.py
+++ api/request/__init__.py
@@ -18,8 +18,9 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     long_url = parse_qs(req_body)["long_url"][0]
 
     #data_to_send = {"long_url": "{long_url}"}
-    request_to_function = requests.post("https://ccurlshortener.azurewebsites.net/api/requestTrigger?code=EnkbZTNMLlnBLurHUj0d8deSqDBs18muyfwAUIJysRvFbjtGtJmqcg==", data=long_url)
-    print(request_to_function.text)
+    #request_to_function = requests.post("https://ccurlshortener.azurewebsites.net/api/requestTrigger?code=EnkbZTNMLlnBLurHUj0d8deSqDBs18muyfwAUIJysRvFbjtGtJmqcg==", data={long_url})
+    #print(request_to_function.text)
+
     return func.HttpResponse(
         f"You submitted this information: {long_url}",
         status_code=200,
